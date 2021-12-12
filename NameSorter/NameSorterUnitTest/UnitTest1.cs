4466e24fe65d29cf682bdbe6a6b8a36f87b6d541
Dionnn <36296966+Dionnn@users.noreply.github.com>
2021-12-12 20:57:02 +0700

    Add files via upload

---
+++ NameSorter/NameSorterUnitTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NameSorter;
+using NameSorter.InterFaces;
+using NameSorter.Implementations;
+
+namespace NameSorterUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCreateMethod()
+        {
+            CreateTxt create = new CreateTxt("test");
+            var result = create.Create();
+        }
+    }
+}
